b9711cea80d7cdb37ef7b1c097c1947d53a9c52e
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-11 20:26:40 -0400

    Add Properties / Getter and Setter / Constructor

--- src/main/java/com/jay/springbootauthentication/model/Role.java
+++ src/main/java/com/jay/springbootauthentication/model/Role.java
@@ -1,4 +1,23 @@
 package com.jay.springbootauthentication.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Data //Getter and Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+    private String name; //name of the role
 }
