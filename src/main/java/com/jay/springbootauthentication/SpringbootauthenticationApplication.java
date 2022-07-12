d4f2f1eb168cecf2b46b0f8248a7ac69a9cf6b13
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-11 20:07:58 -0400

    Initial Commit

---
+++ src/main/java/com/jay/springbootauthentication/SpringbootauthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.jay.springbootauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootauthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootauthenticationApplication.class, args);
+	}
+
+}
