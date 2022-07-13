c9dd6e6b14c48e9f30d853796a06bdc2b7619f34
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-12 22:07:19 -0400

    Update

--- src/main/java/com/jay/springbootauthentication/SpringbootauthenticationApplication.java
+++ src/main/java/com/jay/springbootauthentication/SpringbootauthenticationApplication.java
@@ -1,13 +1,43 @@
 package com.jay.springbootauthentication;
 
+import com.jay.springbootauthentication.model.Role;
+import com.jay.springbootauthentication.model.User;
+import com.jay.springbootauthentication.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class SpringbootauthenticationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootauthenticationApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService) {
+
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null, "John Travolta", "john", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Will Smith", "will", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Jim Carry", "jim", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Arnold Schwarzenegger", "arnold", "1234", new ArrayList<>()));
+
+			userService.addRoleToUser("john", "ROLE_USER");
+			userService.addRoleToUser("will", "ROLE_MANAGER");
+			userService.addRoleToUser("jim", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_USER");
+		};
 	}
 
 }
